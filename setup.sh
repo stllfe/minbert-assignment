9c639dc958bf5e53db84e4fa39d72770f34b5aa0
Oleg Pavlovich <pavlovi4.o@gmail.com>
2024-07-08 12:02:28 +0300

    updates the requirements and the target python version to 3.10

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-conda create -n bert_hw python=3.8
+set -e
+conda create -n bert_hw python=3.10
 conda activate bert_hw
 
-conda install pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
-pip install tqdm==4.58.0
-pip install requests==2.25.1
-pip install importlib-metadata==3.7.0
-pip install filelock==3.0.12
-pip install sklearn==0.0
-pip install tokenizers==0.10.1
-pip install explainaboard_client==0.0.7
+conda install pytorch torchvision torchaudio -c pytorch
+pip install uv &&
+uv pip install tqdm \
+requests \
+importlib-metadata \
+filelock \
+scikit-learn \
+tokenizers \
+explainaboard_client
